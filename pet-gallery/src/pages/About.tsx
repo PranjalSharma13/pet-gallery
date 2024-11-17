c1e9b525e9104d891ffffecb59eafe7196e0a15a
PranjalSharma13 <1SI17CS077@sit.ac.in>
2024-11-17 11:07:43 -0500

    added routing

---
+++ pet-gallery/src/pages/About.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is a sample pet gallery app built using React and TypeScript.</p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
---
+++ pet-gallery/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is a sample pet gallery app built using React and TypeScript.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
